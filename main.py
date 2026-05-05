b8cc4030a257c8c6762d863d322f4cf2e8e17e9b
aaminah26 <aaminahhaniya26@gmail.com>
2026-05-05 12:12:52 +0530

    fixed requirements.txt typo

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
-from dotenv import load_dotenv
-load_dotenv() 
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from database import Base, engine
 from routers import auth, students
+
+from dotenv import load_dotenv
+
 import models.user     
-import models.student 
+import models.student  
+
+load_dotenv()
+
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
+
 Base.metadata.create_all(bind=engine)
+
 app = FastAPI(title="Student Management API", version="1.0.0")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","http://127.0.0.1:5173"],
+    allow_origins=[FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 app.include_router(students.router, prefix="/students", tags=["Students"])
+
 @app.get("/")
 def root():
     return {"message": "Student API is running"}
-
